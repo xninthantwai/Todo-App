6005eb497f21e2c67608500903060c28a6c1dd77
xninthantwai <xninthantwai@gmail.com>
2024-06-26 10:25:49 +0630

    Todo App with Js

---
+++ Todo/app.js
@@ -0,0 +1,27 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.add("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
